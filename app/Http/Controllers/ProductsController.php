740670b89b24b6827883d1b8a40030bbe94893e2
Dendi Handian <dendihandian@gmail.com>
2018-06-03 23:52:06 +0700

    implement elasticsearch in products CRUD

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
@@ -14,7 +15,7 @@ class ProductsController extends Controller
      */
     public function __construct()
     {
-        //
+        $this->elasticsearch = ClientBuilder::create()->build();
     }
 
     public function index()
@@ -25,8 +26,15 @@ class ProductsController extends Controller
 
     public function show($id)
     {
-        $product = Product::findOrFail($id);
-        return response()->json($product, 200);
+        $params = [
+          'index' => Product::INDEX,
+          'type' => 'default',
+          'id' => $id
+        ];
+
+        $response = $this->elasticsearch->get($params);
+
+        return response()->json($response['_source'], 200);
     }
 
     public function store(Request $request)
@@ -40,6 +48,16 @@ class ProductsController extends Controller
         $product->price = $input['price'];
         $product->description = $input['description'];
         $product->save();
+
+        // create new index
+        $params = [
+          'index' => Product::INDEX,
+          'type' => 'default',
+          'id' => $product->id,
+          'body' => $product->toArray(),
+        ];
+
+        $response = $elasticsearch->index($params);
 
         return response()->json($product, 201);
     }
@@ -56,13 +74,45 @@ class ProductsController extends Controller
         $product->description = $input['description'];
         $product->save();
 
+        // delete old index
+        $params = [
+          'index' => Product::INDEX,
+          'type' => 'default',
+          'id' => $product->id,
+        ];
+
+        $response = $elasticsearch->delete($params);
+
+        // create new index
+        $params = [
+          'index' => Product::INDEX,
+          'type' => 'default',
+          'id' => $product->id,
+          'body' => $product->toArray(),
+        ];
+
+        $response = $elasticsearch->index($params);
+
         return response()->json($product, 200);
     }
 
     public function destroy($id)
     {
+        // get product from id
         $product = Product::findOrFail($id);
+
+        // delete index
+        $params = [
+          'index' => Product::INDEX,
+          'type' => 'default',
+          'id' => $product->id,
+        ];
+
+        $response = $elasticsearch->delete($params);
+
+        // delete product
         $product->delete();
+
         return response()->json([], 200);
     }
 }
