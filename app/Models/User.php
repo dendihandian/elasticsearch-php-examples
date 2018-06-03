0e87fcbd36782d927bea24215ec7a618469f24cd
Dendi Handian <dendihandian@gmail.com>
2018-06-03 20:05:04 +0700

    add base model and fix User model extending

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
 use Laravel\Lumen\Auth\Authorizable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
