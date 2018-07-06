c1bceb0d427b19d3de2b25b3e7f857f3e8e5961e
Dendi Handian <handian.dendi@gmail.com>
2018-07-06 22:42:24 +0700

    add getSources function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,4 +7,14 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     protected $elasticsearch;
+
+    protected function getSources($hits)
+    {
+        $sources = [];
+        foreach ($hits['hits']['hits'] as $hit) {
+            array_push($sources, $hit['_source']);
+        }
+
+        return $sources;
+    }
 }
